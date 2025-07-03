74fc2958b6d63715ae29cfe5755534453c0d2c29
AnkithaKaparthi <22311a6704@ds.sreenidhi.edu.in>
2025-07-03 17:17:55 +0530

    Final

---
+++ src/AddModal.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+
+export default function AddModal({ products, setProducts, setShowAddModal, setShow }) {
+
+    const [current, setCurrent] = React.useState({
+        name: '',
+        category: '',
+        stock: ''
+    })
+
+    const handleAdd = ()=>{
+        if(isNaN(current.stock)){
+            alert('Please enter a valid number in stock')
+            return
+        }
+        setProducts(prev=>[...prev, current])
+        setShow(products)
+        alert('Added new product')
+        setTimeout(()=>{
+            setShowAddModal(false)
+        }, 500)
+    }
+
+    return (
+        <div className='w-full h-full absolute top-0 left-0 bg-slate-500 opacity-90 flex items-center justify-center'>
+            <div className='bg-white p-4 rounded-lg w-[300px] space-y-4 opacity-100'>
+                <h2 className='text-lg font-semibold'>Add Product</h2>
+                <div className='flex flex-col space-y-2'>
+                    <label className='text-sm'>Name</label>
+                    <input 
+                        value={current.name} 
+                        className='border p-2 rounded' 
+                        onChange={(e)=>setCurrent(curr=>{return({...curr, name: e.target.value})})}
+                    />
+
+                    <label className='text-sm'>Category</label>
+                    <input 
+                        value={current.category} 
+                        className='border p-2 rounded' 
+                        onChange={(e)=>setCurrent(curr=>{return({...curr, category: e.target.value})})}
+                    />
+
+                    <label className='text-sm'>Stock</label>
+                    <input
+                        value={current.stock}
+                        onChange={(e) =>
+                            setCurrent((curr) => ({
+                                ...curr,
+                                stock: e.target.value
+                            }))
+                        }
+                        className='border p-2 rounded'
+                    />
+                </div>
+                <div className='flex justify-between pt-4'>
+                    <button onClick={() => setShowAddModal(false)} className='px-4 py-2 bg-gray-300 rounded'>Cancel</button>
+                    <button className='px-4 py-2 bg-blue-500 text-white rounded' onClick={()=>handleAdd()}>Add</button>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,114 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import UpdateModal from './UpdateModal';
+import AddModal from './AddModal';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+    const [products, setProducts] = React.useState([
+        { name: "Wireless Headphones", category: "Electronics", stock: 25 },
+        { name: "Blender", category: "Kitchen", stock: 40 },
+        { name: "T-Shirt", category: "Clothing", stock: 100 },
+        { name: "Microwave Oven", category: "Kitchen", stock: 15 },
+        { name: "Smartwatch", category: "Electronics", stock: 30 }
+    ])
+
+    const [showUpdateModal, setShowUpdateModal] = React.useState(false)
+    const [updateIndex, setUpdateIndex] = React.useState(-1)
+
+    const [showAddModal, setShowAddModal] = React.useState(false)
+
+    const [filterCategory, setFilterCatrgory] = React.useState('All')
+
+
+    const [show, setShow] = React.useState(products)
+
+    const handleDelete = (idx)=>{
+        console.log(idx)
+        const newProducts = products.filter((item, i)=>i!==idx)
+        setProducts(newProducts)
+        setShow(products)
+        console.log(products)
+    }
+
+    React.useEffect(()=>{
+        if(filterCategory==='All')  setShow(products)
+        else
+        setShow(products.filter(item=>item.category===filterCategory))
+    }, [filterCategory, products, show])
+
+    return (
+        <div className="flex items-center flex-col">
+            <div className='p-5 bg-orange-600 w-full text-white text-center text-3xl'>E-Commerce Dashboard</div>
+            <div className='w-full'>
+                <div className='text-center w-[10rem] mx-auto text-white p-5 bg-blue-700 rounded-xl m-3 text-2xl'>Products</div>
+                <div 
+                    className='text-center w-fit cursor-pointer mx-auto text-white p-2 bg-green-600 rounded-xl m-3 text-lg'
+                    onClick={()=>setShowAddModal(true)}
+                >
+                    Add product +
+                </div>
+                <div className='flex justify-center gap-3'>
+                    <div className={`p-2 text-md bg-slate-200 rounded-xl cursor-pointer ${filterCategory==='All' && 'bg-slate-400'}`} onClick={()=>setFilterCatrgory('All')}>All</div>
+                    <div className={`p-2 text-md bg-slate-200 rounded-xl  cursor-pointer ${filterCategory==='Electronics' && 'bg-slate-400'}`} onClick={()=>setFilterCatrgory('Electronics')}>Electronics</div>
+                    <div className={`p-2 text-md bg-slate-200 rounded-xl  cursor-pointer ${filterCategory==='Kitchen' && 'bg-slate-400'}`} onClick={()=>setFilterCatrgory('Kitchen')}>Kitchen</div>
+                    <div className={`p-2 text-md bg-slate-200 rounded-xl cursor-pointer  ${filterCategory==='Clothing' && 'bg-slate-400'}`} onClick={()=>setFilterCatrgory('Clothing')}>Clothing</div>
+                </div>
+                <div className='w-[70%] flex mx-auto justify-between m-3 p-3 text-2xl underline'>
+                    <div className='me-5'>Sl.No</div>
+                    <div className='flex-4'>Name</div>
+                    <div className='flex-1'>Category</div>
+                    <div className='flex-1'>Stock</div>
+                    <div className='flex-1'></div>
+                    <div className='flex-1'></div>
+                </div>
+                <div className='w-full flex justify-center flex-col items-center'>
+                    {
+                        show.map((item, i)=>{
+                            return(
+                                <div className='w-[70%] flex justify-between m-3 p-3 bg-slate-400 rounded-xl text-xl'>
+                                    <div className='me-5'>{i+1}</div>
+                                    <div className='flex-4'>{item.name}</div>
+                                    <div className='flex-1'>{item.category}</div>
+                                    <div className='flex-1'>{item.stock}</div>
+                                    <div 
+                                        className='flex-1 text-lg bg-blue-800 text-center text-white rounded-lg cursor-pointer me-2'
+                                        onClick={()=>{
+                                            setShowUpdateModal(true)
+                                            setUpdateIndex(i)
+                                        }}
+                                    >
+                                        Update
+                                    </div>
+                                    <div className='flex-1 text-lg bg-red-500 text-center text-white rounded-lg cursor-pointer' onClick={()=>handleDelete(i)}>Delete</div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+            {
+            showUpdateModal &&
+                <UpdateModal 
+                    setShowUpdateModal={setShowUpdateModal}
+                    index={updateIndex} 
+                    products={products} 
+                    setProducts={setProducts}
+                />
+            }
+            {
+            showAddModal &&
+                <AddModal 
+                    setShowAddModal={setShowAddModal}
+                    setProducts={setProducts}
+                    setShow={setShow}
+                    products={products}
+                />
+            }
+        </div>
+    );
 }
 
 export default App;
